4bcdaa1d733fd75935f8474ff31bb25f8f9548e9
Keisuke Imura <keisuke@keisuke-imura.com>
2016-11-19 18:24:11 +0900

    add test framework

---
+++ wp-meta-tags.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Meta Tags
+Version: 0.1.0
+Description: Output meta description, keywords, opg, twitter cards on your site.
+Author: Keisuke Imura
+Author URI: https://funteractive.co.jp/
+Plugin URI: https://funteractive.co.jp/
+Text Domain: wp-meta-tags
+Domain Path: /languages
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
