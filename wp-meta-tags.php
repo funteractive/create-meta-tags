23128ed111f331142bf88f080743cb331eedc894
Keisuke Imura <keisuke@keisuke-imura.com>
2016-11-19 18:39:29 +0900

    quiet travis

--- wp-meta-tags.php
+++ wp-meta-tags.php
@@ -1,12 +1,13 @@
 <?php
 /**
-Plugin Name: WP Meta Tags
-Version: 0.1.0
-Description: Output meta description, keywords, opg, twitter cards on your site.
-Author: Keisuke Imura
-Author URI: https://funteractive.co.jp/
-Plugin URI: https://funteractive.co.jp/
-Text Domain: wp-meta-tags
-Domain Path: /languages
-License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*/
+ * Plugin Name:     WP Meta Tags
+ * Plugin URI:      https://github.com/funteractive/wp-meta-tags
+ * Description:     Output meta description, keywords, opg, twitter cards on your site.
+ * Author:          Keisuke Imura
+ * Author URI:      https://funteractive.co.jp/
+ * Text Domain:     wp-meta-tags
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WPMetaTags
+ */
