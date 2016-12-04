f3db049451dd922e08395828b1be6f20813530fc
Keisuke Imura <keisuke@keisuke-imura.com>
2016-12-05 08:49:05 +0900

    add find_ogp_data tests

--- tests/models/test-MetaModel.php
+++ tests/models/test-MetaModel.php
@@ -18,6 +18,9 @@ class Test_Models_MetaModel extends \WP_UnitTestCase
     $this->meta_model = new MetaModel();
   }
 
+  /**
+   * find_site_meta
+   */
   public function test_find_site_meta() {
     add_option('create_meta_tags_keywords', self::KEYWORDS);
     add_option('create_meta_tags_description', self::DESCRIPTION);
@@ -39,7 +42,7 @@ class Test_Models_MetaModel extends \WP_UnitTestCase
   }
 
   public function test_find_site_meta_for_single() {
-    $post_id = self::factory()->post->create_and_get([
+    $post = self::factory()->post->create_and_get([
       'post_excerpt' => self::POST_EXCERPT
     ]);
     add_option('create_meta_tags_keywords', self::KEYWORDS);
@@ -47,9 +50,59 @@ class Test_Models_MetaModel extends \WP_UnitTestCase
     $this->assertequals([
       'keywords'    => self::KEYWORDS,
       'description' => self::POST_EXCERPT,
-    ], $this->meta_model->find_site_meta($post_id));
+    ], $this->meta_model->find_site_meta($post->ID));
   }
 
+  /**
+   * find_ogp_data
+   */
+  public function test_find_ogp_data() {
+    $attachment_id = $this->factory->attachment->create_object('image.jpg', 0, [
+      'post_mime_type' => 'image/jpeg',
+      'post_type' => 'attachment'
+    ]);
+    $attachment_src = wp_get_attachment_image_src($attachment_id, 'full');
+    $attachment_src = $attachment_src[0];
+
+    add_option('create_meta_tags_description', self::DESCRIPTION);
+    add_option('create_meta_tags_image', $attachment_id);
+
+    $this->assertequals([
+      'title'       => get_bloginfo('name'),
+      'image'       => $attachment_src,
+      'url'         => home_url(),
+      'description' => self::DESCRIPTION,
+      'site_name'   => get_bloginfo('name'),
+      'type'        => 'website',
+      'locale'      => 'ja_JP',
+    ], $this->meta_model->find_ogp_data());
+  }
+
+  public function test_find_ogp_data_for_single() {
+    $post = self::factory()->post->create_and_get([
+      'post_excerpt' => self::POST_EXCERPT
+    ]);
+    $attachment_id = $this->factory->attachment->create_object('image.jpg', $post->ID, [
+      'post_mime_type' => 'image/jpeg',
+      'post_type' => 'attachment'
+    ]);
+    $attachment_src = wp_get_attachment_image_src($attachment_id, 'full');
+    $attachment_src = $attachment_src[0];
+
+    $this->assertequals([
+      'title'       => get_the_title($post->ID),
+      'image'       => $attachment_src,
+      'url'         => get_permalink($post->ID),
+      'description' => self::POST_EXCERPT,
+      'site_name'   => get_bloginfo('name'),
+      'type'        => 'article',
+      'locale'      => 'ja_JP',
+    ], $this->meta_model->find_ogp_data($post->ID));
+  }
+
+  /**
+   * Other functions.
+   */
   public function test_get_fb_app_id() {
     add_option('create_meta_tags_facebook_app_id', self::FB_APP_ID);
     $this->assertEquals(self::FB_APP_ID, $this->meta_model->get_fb_app_id());
