287e0c946cb6337395ee6414bfdace3c4bf8fafa
Keisuke Imura <keisuke@keisuke-imura.com>
2016-12-28 00:05:16 +0900

    fix readme

--- create-meta-tags.php
+++ create-meta-tags.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Create Meta Tags
  * Plugin URI:      https://github.com/funteractive/wp-meta-tags
- * Description:     Output meta description, keywords, opg, twitter cards on your site.
+ * Description:     Output meta description, keywords, ogp, twitter cards on your site.
  * Author:          Keisuke Imura
  * Author URI:      https://funteractive.co.jp/
  * Text Domain:     create-meta-tags
