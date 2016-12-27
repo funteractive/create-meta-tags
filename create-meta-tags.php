f8e539d30d1c6b17ca90b5dc6f4dcd1b65b56da5
Keisuke Imura <keisuke@keisuke-imura.com>
2016-12-28 00:14:09 +0900

    upgrade 0.1.1

--- create-meta-tags.php
+++ create-meta-tags.php
@@ -7,7 +7,7 @@
  * Author URI:      https://funteractive.co.jp/
  * Text Domain:     create-meta-tags
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         CreateMetaTags
  */
